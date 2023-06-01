e47976d87e848e8c93e3e3add7a4f21b701e465b
Diego178 <zS2001814@estudiantes.uv.mx>
2023-06-01 11:21:29 -0600

    cambios en la estructura

--- rest.js
+++ rest.js
@@ -2,7 +2,26 @@ import axios from 'axios';
 
 export async function obtenerReportes() {
     return new Promise((resolve, reject) => {
-    axios.get('http://127.0.0.1:8000/reportes/')
+    axios.get('http:127.0.0.1:8000/reportes/')
+    .then((response) => {
+        resolve(response.data)
+    }).catch((e) => {
+        reject("Error " + e);
+    })
+})
+}
+
+export async function crearReporte(esp32, mensaje, fecha, hora, latitude, longitude) {
+    return new Promise((resolve, reject) => {
+    axios.post('http://127.0.0.1:8000/reportes/post/',
+    {
+        "esp32": esp32,
+        "mensaje": mensaje,
+        "fecha": fecha,
+        "hora": hora,
+        "latitude": latitude,
+        "longitude": longitude
+    })
     .then((response) => {
         resolve(response.data)
     }).catch((e) => {
